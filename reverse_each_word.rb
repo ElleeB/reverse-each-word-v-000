775afa6dedbc04a8f433e84d47e476984e200337
Laura Trager <laura.trager@gmail.com>
2017-10-30 14:29:30 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,13 @@
 def reverse_each_word(string = "Hello there, and how are you?")
+  final_array = []
+  array = string.split(" ")
+  new_array = array.each do |word|
+    final_array << word.reverse
+  end
+  final_array.join(" ")
+end
+
+def reverse_each_word(string = "Hello there, and how are you?")
   array = string.split(" ")
   new_array = array.collect do |word|
     word.reverse
